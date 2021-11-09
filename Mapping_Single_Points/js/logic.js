4115e606de9f56b5792276debd3e858c0920b05a
Joelle Ousley <jody@Joelles-Air.localdomain>
2021-11-09 07:53:36 -0600

    add updated logic

--- Mapping_Single_Points/js/logic.js
+++ Mapping_Single_Points/js/logic.js
@@ -13,7 +13,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 
 //  Add a marker to the map for Los Angeles, California.
 L.circle([34.0522, -118.2437], {
-    radius: 100,
+    radius: 300,
     color: "black",
     fillColor: '#ffffa1'
  }).addTo(map);
